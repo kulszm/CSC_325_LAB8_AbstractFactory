343dfbedd53ed5ba149e0ccbde788a9d20f53bc1
kulsomzaraei <kulsomzaraei@macbook-air>
2022-10-24 23:10:16 -0400

    Professional pants style class that implements pants

---
+++ CSC325_LAB8_AbstractFactory/src/main/java/ProfessionalPants.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author kulsomzaraei
+ */
+public class ProfessionalPants implements Pants {
+    
+    @Override
+    public void pantsStyle(){
+        System.out.println("These pants are professional style");
+    }
+    
+}
