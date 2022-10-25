985cb1fc727f348829b28b935e8e2973427f0259
kulsomzaraei <kulsomzaraei@macbook-air>
2022-10-24 23:19:04 -0400

    party Shoes style class that implements shoes

---
+++ CSC325_LAB8_AbstractFactory/src/main/java/PartyShoes.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author kulsomzaraei
+ */
+public class PartyShoes implements Shoes {
+    
+    @Override
+    public void ShoesStyle(){
+        System.out.println("These shoes are party style");
+    }
+    
+}
